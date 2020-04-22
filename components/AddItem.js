ff690191299e489edae860db725edc5317dad35d
Don-Stevenson <dsteven1@gmail.com>
2020-04-21 22:32:23 -0400

    set up xml for AddItem

--- components/AddItem.js
+++ components/AddItem.js
@@ -1,17 +1,26 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity
+} from "react-native";
+import Icon from "react-native-vector-icons/dist/FontAwesome";
 
-const AddItem = ({title}) => {
+const AddItem = ({ title }) => {
   return (
-   
+    <View>
+      <TextInput placeholder="Add Item.." style={styles.input} />
+      <TouchableOpacity style={styles.btn}>
+        <Text style={styles.btnText}>
+          <Icon name="plus" size={20} /> Add Item
+        </Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 
-
-
-const styles = StyleSheet.create({
-
-
-});
+const styles = StyleSheet.create({});
 
 export default AddItem;
